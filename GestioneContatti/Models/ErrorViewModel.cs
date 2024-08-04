1c303a068860b6b448b84fbf342115bd3e63a319
Luca-pintimalli <163635386+Luca-pintimalli@users.noreply.github.com>
2024-08-04 13:19:10 +0200

    Add files via upload
    Creazione cartella

---
+++ GestioneContatti/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GestioneContatti.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
